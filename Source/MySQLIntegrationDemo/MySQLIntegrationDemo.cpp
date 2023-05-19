75e6a5a9e43a2a7c6c75040bac9c9443d4d5604e
Sameek <sameek.kundu@athiangames.com>
2023-05-20 01:19:05 +0530

    Added example projects for 5.1 and 5.2

--- Source/MySQLIntegrationDemo/MySQLIntegrationDemo.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MySQLIntegrationDemo.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MySQLIntegrationDemo, "MySQLIntegrationDemo" );
