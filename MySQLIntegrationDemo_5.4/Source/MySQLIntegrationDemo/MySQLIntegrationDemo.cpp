a969b478540c1df2c0e685b71a5114e61f710421
SAMEEKK_LAPTOP\samee <sameek.kundu@athiangames.com>
2024-05-04 16:12:50 +0530

    Added for 5.4

---
+++ MySQLIntegrationDemo_5.4/Source/MySQLIntegrationDemo/MySQLIntegrationDemo.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "MySQLIntegrationDemo.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MySQLIntegrationDemo, "MySQLIntegrationDemo" );
---
+++ MySQLIntegrationDemo_5.4/Source/MySQLIntegrationDemoEditor.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class MySQLIntegrationDemoEditorTarget : TargetRules
+{
+	public MySQLIntegrationDemoEditorTarget( TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Editor;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		ExtraModuleNames.Add("MySQLIntegrationDemo");
+	}
+}
